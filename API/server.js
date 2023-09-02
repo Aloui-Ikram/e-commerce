73a80cee00ffb45c7e44d56aa6ea46b5965fe1ec
Ikram ALOUI <109230617+Aloui-Ikram@users.noreply.github.com>
2023-09-02 05:49:22 -1100

    Create server.js

---
+++ API/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
